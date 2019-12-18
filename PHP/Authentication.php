38c3146c16dee15182e2b7205d24bba465e38153
Minh-Thong <53897381+Minh-Thong@users.noreply.github.com>
2019-12-18 14:01:37 +0700

    Update Authentication.php

--- PHP/Authentication.php
+++ PHP/Authentication.php
@@ -1,11 +1,16 @@
-<?php
 
-/*session_start();
- if(!isset($_SESSION["loggedin"])){
- header('Location:Login.html');
- exit();
- }
- */
+
+<div class="ShowError" align="center" style=" border-radius: 30px; text-align: center; width: 300px; height: 150px; background: #8E8B8B; color: white; margin: 0 auto;">
+	<h1> Thông Báo !!!</h1>
+	<br>
+	<?php
+
+//session_start();
+// if(!isset($_SESSION["loggedin"])){
+// header('Location:..Login.html');
+// exit();
+// }
+
 if(!isset($_POST['Username'],$_POST['Password'])){
     die('Vui lòng nhập User và Password');
 }
@@ -19,16 +24,27 @@ $con = mysqli_connect($Database_host,$Database_user,$Database_pass,$Database_nam
 $username = mysqli_real_escape_string($con, $_POST['Username']);
 $password = mysqli_real_escape_string($con, $_POST['Password']);
 
-$query = "SELECT * FROM account WHERE ID='$username' AND Password='$password'";
+$query = "SELECT Password FROM account WHERE ID='$username'";
 $results = mysqli_query($con, $query);
 
-if (mysqli_num_rows($results) == 1) {
-    $_SESSION["Username"] =$username;
-    header('Location:../Home.html');
+if (mysqli_num_rows($results) == 0) {
+	echo "Tai khoan khong ton tai";
 }
-else {
-    //Dang nhap that bai
-    header('Location:../Login.html');
+if(mysqli_num_rows($results) == 1){
+	while($row = mysqli_fetch_assoc($results)) {
+		if($row['Password'] == $_POST['Password']){
+			$_SESSION["Username"] = $username;
+			$_SESSION["Type"] = $row['type'];
+			header('Location:../Home.html');
+		}else {
+    		echo "Mat khau khong dung";
+//   			header('Location:../Login.html');
+		}
+	}
 }
-
 ?>
+	<br>	
+	<button onClick="location.href='../login.html'" type="button" style="width: 50px;
+																		 height: 20px; margin-top: 20px; background: none;
+																		 border: 1px solid #F7F6F6"> Ok</button>
+</div>
